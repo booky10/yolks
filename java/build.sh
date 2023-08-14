cf4f6f7df81fa1600ed3b3c87ce7e8d8c62d95b7
booky10 <boooky10@gmail.com>
2023-08-14 08:32:22 +0200

    Adjust building scripts to always use the correct image

--- java/build.sh
+++ java/build.sh
@@ -7,9 +7,14 @@ BASE_NAME="$(basename $PWD)"
 for ((i = 0; i < ${#VERSIONS[@]}; i++)); do
     TAG="$TAG_BASE:$BASE_NAME${VERSIONS[i]}"
     DIR="$PWD/${VERSIONS[i]}"
+    DOCKERFILE="$DIR/Dockerfile"
+
+    IMAGE=$(cat "$DOCKERFILE" | grep "FROM" | awk ' { print $3 } ')
+    echo "Checking for updates on image $IMAGE..."
+    docker pull "$IMAGE"
 
     echo "Building $BASE_NAME v${VERSIONS[i]} (tag: $TAG)..."
-    docker build --tag "$TAG" --file "$DIR/Dockerfile" "$PWD"
+    docker build --tag "$TAG" --file "$DOCKERFILE" "$PWD"
 
     if [ ! -z "$PUBLISH_IMAGES" ]; then
         echo "Pushing $BASE_NAME v${VERSIONS[i]} (tag: $TAG)..."
--- python/build.sh
+++ python/build.sh
@@ -5,11 +5,18 @@ declare -a VERSIONS=("3.8" "3.9" "3.10" "3.11")
 
 BASE_NAME="$(basename $PWD)"
 for ((i = 0; i < ${#VERSIONS[@]}; i++)); do
+    docker pull "python:${VERSIONS[i]}-alpine"
+
     TAG="$TAG_BASE:$BASE_NAME${VERSIONS[i]}"
     DIR="$PWD/${VERSIONS[i]}"
+    DOCKERFILE="$DIR/Dockerfile"
+
+    IMAGE=$(cat "$DOCKERFILE" | grep "FROM" | awk ' { print $3 } ')
+    echo "Checking for updates on image $IMAGE..."
+    docker pull "$IMAGE"
 
     echo "Building $BASE_NAME v${VERSIONS[i]} (tag: $TAG)..."
-    docker build --tag "$TAG" --file "$DIR/Dockerfile" "$PWD"
+    docker build --tag "$TAG" --file "$DOCKERFILE" "$PWD"
 
     if [ ! -z "$PUBLISH_IMAGES" ]; then
         echo "Pushing $BASE_NAME v${VERSIONS[i]} (tag: $TAG)..."
